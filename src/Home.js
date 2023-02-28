ca12fe47c6c09840ae51989562c6ca45e74dfa4d
gigilaw <gigi.law.14@gmail.com>
2023-02-28 15:07:19 +0800

    feat(Home): add fetching logic and save to local state

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,25 @@
+import { useEffect, useState } from 'react';
+
 const Home = () => {
+  const [allFriends, setAllFriends] = useState(null);
+
+  useEffect(() => {
+    const headers = {
+      Authorization: 'Bearer ' + process.env.REACT_APP_ALL_FRIENDS_TOKEN,
+    };
+
+    fetch(process.env.REACT_APP_ALL_FRIENDS_ENDPOINT, {
+      headers,
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setAllFriends(data);
+      });
+  }, []);
+
   return (
     <div className="home">
       <h1>All Friends</h1>
