4b7338037095b8c2ec377ff0d741da870011c3aa
gigilaw <gigi.law.14@gmail.com>
2023-03-01 10:25:39 +0800

    feat(AllFriendsList): update css and title

--- src/AllFriendsList.js
+++ src/AllFriendsList.js
@@ -1,7 +1,7 @@
 const AllFriendsList = ({ allFriends }) => {
   return (
     <div className="friends-list">
-      <h1>All Friends</h1>
+      <h1>My Bear-ly Friends</h1>
 
       {allFriends.map((friend) => (
         <div className="friends-preview" key={friend._id}>
@@ -9,7 +9,10 @@ const AllFriendsList = ({ allFriends }) => {
             src={friend.picture}
             alt={`${friend.name.first} ${friend.name.last}`}
           ></img>
-          <h2>{`${friend.name.first} ${friend.name.last}`}</h2>
+          <div className="a">
+            <h2>{`${friend.name.first} ${friend.name.last}`}</h2>
+            <p>See more...</p>
+          </div>
         </div>
       ))}
     </div>
