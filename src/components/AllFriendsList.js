d37031fde733445d76579fcf66dd16dd110e4071
gigilaw <gigi.law.14@gmail.com>
2023-03-03 13:19:14 +0800

    feat(AllFriendsList): update to responsive grid style

--- src/components/AllFriendsList.js
+++ src/components/AllFriendsList.js
@@ -1,28 +1,37 @@
 import { Link } from 'react-router-dom';
+import { Avatar, Grid } from '@mui/material';
 
 const AllFriendsList = ({ allFriends }) => {
   return (
-    <div className="friends-list">
+    <div>
       <h1>My Bear-ly Friends!</h1>
-
-      {allFriends.map((friend) => (
-        <div key={friend._id}>
-          <Link to={`/friends/${friend._id}`} state={{ friend }}>
-            <div className="friends-preview">
-              <img
-                src={friend.picture}
-                alt={`${friend.name.first} ${friend.name.last}`}
-              ></img>
-              <div className="friends-text">
-                <h2>
-                  {friend.name.first} {friend.name.last}
-                </h2>
-                <p>See more...</p>
-              </div>
-            </div>
-          </Link>
-        </div>
-      ))}
+      <div className="friends-list">
+        {allFriends.map((friend) => (
+          <Grid container>
+            <Grid item xs={12} className="friends-preview">
+              <Link to={`/friends/${friend._id}`} state={{ friend }}>
+                <Grid container>
+                  <Grid item xs={12} sm={3}>
+                    <Avatar
+                      alt={`${friend.name.first} ${friend.name.last}`}
+                      src={friend.picture}
+                      sx={{ width: 100, height: 100 }}
+                    />
+                  </Grid>
+                  <Grid item xs={12} sm={9}>
+                    <div>
+                      <h2>
+                        {friend.name.first} {friend.name.last}
+                      </h2>
+                      <p>See more...</p>
+                    </div>
+                  </Grid>
+                </Grid>
+              </Link>
+            </Grid>
+          </Grid>
+        ))}
+      </div>
     </div>
   );
 };
